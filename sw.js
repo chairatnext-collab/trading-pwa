b08ab9dc19b0e553741694e53d0f4299d472b766
chairatnext-collab <chairat.next@gmail.com>
2026-08-14 23:20:14 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'trading-pwa-v22';
+const CACHE = 'trading-pwa-v23';
 const SHELL = ['./', './index.html', './manifest.json',
                  'https://cdn.jsdelivr.net/npm/chart.js@4.5.0/dist/chart.umd.js'];
 
